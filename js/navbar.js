0d5481a33fcc55793e60c3b92995d6b8502ba70a
toofastdan117 <81544118+toofastdan117@users.noreply.github.com>
2023-12-31 21:45:24 -0700

    main sections started

--- js/navbar.js
+++ js/navbar.js
@@ -3,9 +3,31 @@
 // Getting the navbar elements
 const toggleButton = document.getElementById('toggleButton');
 const navLinks = document.getElementById('navLinks');
+const socialMedias = document.getElementById('socialMediaList');
+const lastNavLink = document.getElementById('lastLink');
+let checkActive = false;
 
 // Toggling the mobile dropdown menu on/off and animating the hamburger menu onclick
 toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  toggleButton.classList.toggle('open');
-});
+  if (!checkActive) {
+    navLinks.classList.add('active');
+    socialMedias.classList.add('active');
+    toggleButton.classList.add('open');
+    lastNavLink.innerHTML = socialMedias.innerHTML;
+    checkActive = true;
+  } else {
+    navLinks.classList.remove('active');
+    socialMedias.classList.remove('active');
+    toggleButton.classList.remove('open');
+    lastNavLink.innerHTML = '';
+    checkActive = false;
+  }
+})
+
+
+//toggleButton.addEventListener('click', () => {
+//  navLinks.classList.toggle('active');
+//  socialMedias.classList.toggle('active');
+//  lastNavLink.innerHTML = socialMedias.innerHTML;
+//  toggleButton.classList.toggle('open');
+//});
