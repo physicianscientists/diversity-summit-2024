375994c647cc5f5de9af5d709c68bdc87b019d68
toofastdan117 <81544118+toofastdan117@users.noreply.github.com>
2023-12-31 18:32:29 -0700

    mobile header

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,11 @@
-// This is for collapsing the mobile navbar
+// This is for toggling the mobile navbar
+
+// Getting the navbar elements
+const toggleButton = document.getElementById('toggleButton');
+const navLinks = document.getElementById('navLinks');
+
+// Toggling the mobile dropdown menu on/off and animating the hamburger menu onclick
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  toggleButton.classList.toggle('open');
+});
